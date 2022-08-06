5dfad6dcfc3a6246c501c94158c4566bebc32d09
Pardeep Gahlawat <gahlawat80@hotmail.com>
2022-08-06 18:15:24 -0400

    get checkout details api

--- src/main/java/com/learn2code/mycar/buy/app/service/CheckoutService.java
+++ src/main/java/com/learn2code/mycar/buy/app/service/CheckoutService.java
@@ -1,7 +1,12 @@
 package com.learn2code.mycar.buy.app.service;
 
 import com.learn2code.mycar.buy.app.dto.CheckoutRequest;
+import com.learn2code.mycar.buy.app.entity.Checkout;
+
+import java.util.List;
 
 public interface CheckoutService {
     String saveCarDetailsToCheckout(CheckoutRequest req);
+
+    List<Checkout> fetchAllItemsForCustomer(String customerCode);
 }
