d1ff749b5f95b5b20cb419e4b74d02b426642664
Pardeep Gahlawat <gahlawat80@hotmail.com>
2022-08-06 15:33:44 -0400

    api setup creation

---
+++ src/main/java/com/learn2code/mycar/buy/app/CarPurchaseAppApplication.java
@@ -0,0 +1,13 @@
+package com.learn2code.mycar.buy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarPurchaseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarPurchaseAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn2code/mycar/buy/app/CarPurchaseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn2code.mycar.buy.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPurchaseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
